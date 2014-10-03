44d7792a194c600933c78f5c8874bdcf97c67eb0
Rishi Goomar <rishig92@gmail.com>
2014-10-03 15:18:13 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'rgoomar:contactform',
   summary: 'Simple drop-in contact form with validation',
-  version: '0.0.5',
+  version: '0.0.5_1',
   git: 'https://github.com/rgoomar/contactform'
 });
 
